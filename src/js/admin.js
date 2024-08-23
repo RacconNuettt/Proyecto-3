99aac79cc20db9c1e956762a42c7c6311497b4aa
greengoblin77 <jgonzalez@fwdcostarica.com>
2024-08-22 21:20:51 -0600

    terminar toda esta p

---
+++ src/js/admin.js
@@ -0,0 +1,8 @@
+/* document.querySelector('.cart-icon').addEventListener('click', () => {
+    document.getElementById('cart-popup').style.display = 'flex';
+});
+document.querySelector('.cart-popup .close').addEventListener('click', () => {
+    document.getElementById('cart-popup').style.display = 'none';
+});
+ */
+
